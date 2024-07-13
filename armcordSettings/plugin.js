4f16e5e76c86b9ca0a7ef1b0d9b26d589803cbbd
smartfrigde <smartfrigde@users.noreply.github.com>
2024-07-13 15:44:21 +0000

    deploy: 3788f8b3cf9bba95a45b5a95759a936a7d722a1a

---
+++ armcordSettings/plugin.js
@@ -0,0 +1,186 @@
+(() => {
+  var __create = Object.create;
+  var __defProp = Object.defineProperty;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __getProtoOf = Object.getPrototypeOf;
+  var __hasOwnProp = Object.prototype.hasOwnProperty;
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+  var __export = (target, all) => {
+    for (var name in all)
+      __defProp(target, name, { get: all[name], enumerable: true });
+  };
+  var __copyProps = (to, from, except, desc) => {
+    if (from && typeof from === "object" || typeof from === "function") {
+      for (let key of __getOwnPropNames(from))
+        if (!__hasOwnProp.call(to, key) && key !== except)
+          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+    }
+    return to;
+  };
+  var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+    // If the importer is in node compatibility mode or this is not an ESM
+    // file that has been converted to a CommonJS file using a Babel-
+    // compatible transform (i.e. "__esModule" has not been set), then set
+    // "default" to the CommonJS "module.exports" for node compatibility.
+    isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+    mod
+  ));
+  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+  // shltr-res-ns:solid-js/web
+  var require_web = __commonJS({
+    "shltr-res-ns:solid-js/web"(exports, module) {
+      module.exports = shelter.solidWeb;
+    }
+  });
+
+  // plugins/armcordSettings/index.js
+  var armcordSettings_exports = {};
+  __export(armcordSettings_exports, {
+    onLoad: () => onLoad,
+    onUnload: () => onUnload
+  });
+
+  // plugins/armcordSettings/pages/LegacyPage.jsx
+  var import_web = __toESM(require_web(), 1);
+  var {
+    ui: {
+      Header,
+      HeaderTags,
+      Button,
+      Divider
+    }
+  } = shelter;
+  function LegacyPage() {
+    return [(0, import_web.createComponent)(Header, {
+      get tag() {
+        return HeaderTags.H1;
+      },
+      children: "Legacy ArmCord components"
+    }), (0, import_web.createComponent)(Button, {
+      get onClick() {
+        return window.armcord.openSettingsWindow;
+      },
+      children: "Settings"
+    }), (0, import_web.createComponent)(Divider, {
+      mt: true,
+      mb: "30px"
+    }), (0, import_web.createComponent)(Button, {
+      get onClick() {
+        return window.armcord.openThemesWindow;
+      },
+      children: "Themes"
+    })];
+  }
+
+  // plugins/armcordSettings/pages/SettingsPage.jsx
+  var import_web2 = __toESM(require_web(), 1);
+  var {
+    plugin: {
+      store
+    },
+    ui: {
+      SwitchItem,
+      Header: Header2,
+      Divider: Divider2,
+      HeaderTags: HeaderTags2
+    }
+  } = shelter;
+  async function SettingsPage() {
+    const settings = armcord.settings.config;
+    console.error(settings);
+    return [(0, import_web2.createComponent)(Header2, {
+      get tag() {
+        return HeaderTags2.H1;
+      },
+      children: "Settings"
+    }), (0, import_web2.createComponent)(Divider2, {
+      mt: true,
+      mb: "30px"
+    }), (0, import_web2.createComponent)(Header2, {
+      get tag() {
+        return HeaderTags2.H5;
+      },
+      children: "Mods"
+    }), (0, import_web2.createComponent)(SwitchItem, {
+      note: "placeholder",
+      get value() {
+        return settings.armcordCSP;
+      },
+      children: "ArmCord CSP"
+    }), (0, import_web2.createComponent)(SwitchItem, {
+      note: "placeholder",
+      get value() {
+        return settings.autoScroll;
+      },
+      children: "Vencord"
+    })];
+  }
+
+  // plugins/armcordSettings/pages/ThemesPage.jsx
+  var import_web3 = __toESM(require_web(), 1);
+  var {
+    plugin: {
+      store: store2
+    },
+    ui: {
+      TextBox,
+      Header: Header3,
+      HeaderTags: HeaderTags3
+    }
+  } = shelter;
+  function ThemesPage() {
+    return (0, import_web3.createComponent)(Header3, {
+      get tag() {
+        return HeaderTags3.H1;
+      },
+      children: "Themes"
+    });
+  }
+
+  // plugins/armcordSettings/pages/UpdaterPage.jsx
+  var import_web4 = __toESM(require_web(), 1);
+  var {
+    plugin: {
+      store: store3
+    },
+    ui: {
+      TextBox: TextBox2,
+      Header: Header4,
+      HeaderTags: HeaderTags4
+    }
+  } = shelter;
+  function UpdaterPage() {
+    return (0, import_web4.createComponent)(Header4, {
+      get tag() {
+        return HeaderTags4.H1;
+      },
+      children: "Updater"
+    });
+  }
+
+  // plugins/armcordSettings/index.js
+  var {
+    settings: { registerSection },
+    util: { log }
+  } = shelter;
+  var settingsPages = [
+    registerSection("divider"),
+    registerSection("header", "ArmCord"),
+    registerSection("section", "armcord-legacy", "Legacy", LegacyPage),
+    registerSection("section", "armcord-settings", "Settings", SettingsPage),
+    registerSection("section", "armcord-themes", "Themes", ThemesPage),
+    registerSection("section", "armcord-updater", "Updater", UpdaterPage)
+  ];
+  function onLoad() {
+    log("ArmCord Settings");
+    settingsPages;
+  }
+  function onUnload() {
+    settingsPages.forEach((e) => e());
+  }
+  return __toCommonJS(armcordSettings_exports);
+})();
