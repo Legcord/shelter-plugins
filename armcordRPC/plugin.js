ba1f681b63669476a4a19f1a71b398378b1c628f
smartfrigde <smartfrigde@users.noreply.github.com>
2024-07-14 20:41:16 +0000

    deploy: 03b4065ed132255361383153df73f58594053f2d

--- armcordRPC/plugin.js
+++ armcordRPC/plugin.js
@@ -20,19 +20,50 @@
   // plugins/armcordRPC/index.js
   var armcordRPC_exports = {};
   __export(armcordRPC_exports, {
+    fetchAssetId: () => fetchAssetId,
     onLoad: () => onLoad
   });
   var {
     util: { log },
     flux: {
       dispatcher: FluxDispatcher,
-      stores: { ApplicationStore, DetectableGameSupplementalStore }
-    }
+      stores: { ApplicationStore }
+    },
+    http
   } = shelter;
   var apps = {};
+  var assetCache = {};
+  var fetchAssetId = async (applicationId, assetName) => {
+    if (!assetCache[applicationId]) {
+      try {
+        const response = await http.get(`/oauth2/applications/${applicationId}/assets`);
+        if (response.status !== 200) {
+          console.error("Error fetching resources");
+          return null;
+        }
+        assetCache[applicationId] = response.body;
+      } catch (error) {
+        console.error("Request failed", error);
+        return null;
+      }
+    }
+    const resource = assetCache[applicationId].find((item) => item.name === assetName);
+    return resource ? resource.id : null;
+  };
   function onLoad() {
     ArmCordRPC.listen(async (msg) => {
+      console.log(msg);
       log("ArmCord RPC: detected some game");
+      if (msg.activity?.assets?.large_image)
+        msg.activity.assets.large_image = await fetchAssetId(
+          msg.activity.application_id,
+          msg.activity.assets.large_image
+        );
+      if (msg.activity?.assets?.small_image)
+        msg.activity.assets.small_image = await fetchAssetId(
+          msg.activity.application_id,
+          msg.activity.assets.small_image
+        );
       if (msg.activity) {
         const appId = msg.activity.application_id;
         if (!apps[appId])
